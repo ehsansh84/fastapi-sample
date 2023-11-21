5fe6b5a2234d16d8ff95d074b8dc9bd1c7fe023b
Ehsan <ehsan.shirzadi@gmail.com>
2023-11-21 12:58:26 +0100

    updated: output status

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
-import uuid
 from pydantic import BaseModel
 from fastapi import APIRouter, status, HTTPException, Request
-
+from api_models.sensor import Read, Write, Update, ListRead
+from api_models.general import Item, OutputOnlyNote, OutputCreate
 
 app = FastAPI()
 
@@ -11,61 +11,56 @@ app = FastAPI()
 sensors = []
 
 
-class Sensor(BaseModel):
-    uid: str = ""
-    name: str = ""
-    status: str = ""
-    location: dict = {}
+#TODO: Define constants for sensor states
 
-    def __init__(self, **data):
-        super().__init__(**data)
-        self.uid = str(uuid.uuid4())
-
-    def __str__(self):
-        attributes = vars(self)
-        return ", ".join(f"{key}: {value}" for key, value in attributes.items())
-
-
-@app.post("/sensor/")
-async def create_item(item: Sensor):
+@app.post("/sensor/", status_code=status.HTTP_201_CREATED, response_model=OutputCreate)
+async def create_item(item: Write):
     sensors.append(item)
     print(sensors)
-    return {"id": item.uid}
+    return {"data": {"uid": item.uid}, "detail": "Item successfully created."}
 
 
-@app.get("/sensor/")
+@app.get("/sensor/", status_code=status.HTTP_200_OK, response_model=ListRead)
 async def read_item():
     return sensors
 
 
-@app.get("/sensor/{uid}")
+@app.get("/sensor/{uid}", status_code=status.HTTP_200_OK, response_model=Read)
 async def read_item(uid: str):
     for item in sensors:
         if item.uid == uid:
             return item
     raise HTTPException(
-        status_code=status.HTTP_409_CONFLICT,
+        status_code=status.HTTP_404_NOT_FOUND,
         detail=f'Record not found!',
     )
 
 
-@app.put("/sensor/{uid}")
-async def update_item(uid: str, item: Sensor):
+@app.put("/sensor/{uid}", status_code=status.HTTP_201_CREATED, response_model=OutputOnlyNote)
+async def update_item(uid: str, item: Update):
     print(item.uid)
-
+    item.uid = uid
+    for idx, sensor in enumerate(sensors):
+        if sensor.uid == uid:
+            sensors[idx] = item
     raise HTTPException(
-        status_code=status.HTTP_409_CONFLICT,
+        status_code=status.HTTP_404_NOT_FOUND,
         detail=f'Record not found!',
     )
 
 
-@app.delete("/sensor/{uid}")
+@app.delete("/sensor/{uid}", status_code=status.HTTP_200_OK, response_model=OutputOnlyNote)
 async def delete_item(uid: str):
     for item in sensors:
         if item.uid == uid:
             sensors.remove(item)
-    print(sensors)
-    return 1
+            print(sensors)
+            return 'Item successfully deleted.'
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'Record not found!',
+    )
 
 
 if __name__ == "__main__":
