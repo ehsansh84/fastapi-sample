0160daf60fb9bb271db70b978e66688f61872d52
Ehsan <ehsan.shirzadi@gmail.com>
2023-11-21 16:12:27 +0100

    updated: Update class to remove unused filed of uid

--- api_models/sensor.py
+++ api_models/sensor.py
@@ -43,8 +43,6 @@ class Update(Base):
     f"""
     Use this model to update a Sensor
     """
-    # In case of a real database we do not need this field
-    uid: str = Field(description="uid of Sensor", example="f969d7c6-c9a0-470b-84bf-b286f71ca38b")
-
+    pass
 
 ListRead = List[Read]
