f0928975ba01366a693b23ad57aff34ca64d3167
Ehsan <ehsan.shirzadi@gmail.com>
2023-11-21 12:47:08 +0100

    removed: task classes

--- api_models/general.py
+++ api_models/general.py
@@ -2,12 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class Item(BaseModel):
-    id: str = Field(description="Id of created object", example="63941df5987d017a2a38f762")
-
-
-class ItemTask(BaseModel):
-    id: str = Field(description="Id of created object", example="63941df5987d017a2a38f762")
-    task_id: str = Field(description="Id of task created in celery", example="63941df5987d017a2a38f762")
+    uid: str = Field(description="uid of created object", example="f969d7c6-c9a0-470b-84bf-b286f71ca38b")
 
 
 class OutputCreate(BaseModel):
@@ -15,19 +10,11 @@ class OutputCreate(BaseModel):
     Use this model to create method response_model
     """
     data: Item
-    detail: str = Field(description="Note string")
-
-
-class OutputCreateTask(BaseModel):
-    """
-    Use this model to create method response_model
-    """
-    data: ItemTask
-    detail: str = Field(description="Note string")
+    detail: str = Field(description="Item successfully created.")
 
 
 class OutputOnlyNote(BaseModel):
     """
-    Use this model to all methods with only a text a output for response_model
+    Use this model to all methods with only a text an output for response_model
     """
     detail: str = Field(description="Note string")
