194e86006e6390451cb49ada37dfaf95d4d30ece
Ehsan <ehsan.shirzadi@gmail.com>
2023-11-21 12:40:26 +0100

    added: general.py

---
+++ api_models/general.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field
+
+
+class Item(BaseModel):
+    id: str = Field(description="Id of created object", example="63941df5987d017a2a38f762")
+
+
+class ItemTask(BaseModel):
+    id: str = Field(description="Id of created object", example="63941df5987d017a2a38f762")
+    task_id: str = Field(description="Id of task created in celery", example="63941df5987d017a2a38f762")
+
+
+class OutputCreate(BaseModel):
+    """
+    Use this model to create method response_model
+    """
+    data: Item
+    detail: str = Field(description="Note string")
+
+
+class OutputCreateTask(BaseModel):
+    """
+    Use this model to create method response_model
+    """
+    data: ItemTask
+    detail: str = Field(description="Note string")
+
+
+class OutputOnlyNote(BaseModel):
+    """
+    Use this model to all methods with only a text a output for response_model
+    """
+    detail: str = Field(description="Note string")
